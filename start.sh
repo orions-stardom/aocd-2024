0e24c59ddfcd527a32aba9f79f313fbcb3308612
Lennon Cook <lvc@codetripping.net>
2024-12-01 22:25:56 +1100

    Update the boilerplate to work a bit smoother

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 filename=day_$(date +%d).py
 cp --update=none template.py $filename
+chmod +x $filename
 pdm run nvim $filename
--- template.py
+++ template.py
@@ -6,23 +6,24 @@ def part_1(rawdata):
 def part_2(rawdata):
     pass
 
-from aocd import puzzle
+from aocd import puzzle, submit
 import pytest
 
 @pytest.mark.parametrize("data, result",
-     [(ex.input_data, ex.answer_a) for ex in aocd.puzzle.examples])
+     [(ex.input_data, ex.answer_a) for ex in puzzle.examples])
 def test_part_1(data, result):
     assert part_1(data) == result
 
 @pytest.mark.parametrize("data, result",
-     [(ex.input_data, ex.answer_b) for ex in aocd.puzzle.examples])
+     [(ex.input_data, ex.answer_b) for ex in puzzle.examples])
 def test_part_2(data, result):
     assert part_2(data) == result
 
 pytest.main([__file__])
 
-puzzle.answer_a = part_1(puzzle.input_data)
+part_1_done = puzzle.answered_a
+submit(part_1(puzzle.input_data), part="a", reopen=False)
 
-if puzzle.answered_a:
-    puzzle.answer_b = part_2(puzzle.input_data)
-
+if part_1_done:
+    # Don't try part 2 yet if we only finished part 1 just now
+    submit(part_2(puzzle.input_data), part="b", reopen=False)
