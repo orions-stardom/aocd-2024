ceeb085853b806a374df794221a61db0512b5f18
Lennon Cook <lvc@codetripping.net>
2024-12-02 16:29:14 +1100

    More template betterification

--- template.py
+++ template.py
@@ -8,6 +8,7 @@ def part_2(rawdata):
 
 from aocd import puzzle, submit
 import pytest
+import sys
 
 @pytest.mark.parametrize("data, result",
      [(ex.input_data, ex.answer_a) for ex in puzzle.examples])
@@ -19,11 +20,13 @@ def test_part_1(data, result):
 def test_part_2(data, result):
     assert part_2(data) == result
 
-pytest.main([__file__])
+if __name__ == "__main__":
+    if (test_result := pytest.main([__file__])):
+        sys.exit(test_result)
 
-part_1_done = puzzle.answered_a
-submit(part_1(puzzle.input_data), part="a", reopen=False)
+    part_1_done = bool(puzzle.answered_a)
+    submit(part_1(puzzle.input_data), part="a", reopen=False)
 
-if part_1_done:
-    # Don't try part 2 yet if we only finished part 1 just now
-    submit(part_2(puzzle.input_data), part="b", reopen=False)
+    if part_1_done:
+        # Don't try part 2 yet if we only finished part 1 just now
+        submit(part_2(puzzle.input_data), part="b", reopen=False)
