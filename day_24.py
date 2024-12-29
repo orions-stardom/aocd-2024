d118d6b8845e8608c8fee45d1f6a530b43e71163
Lennon Cook <lvc@codetripping.net>
2024-12-29 21:21:42 +1100

    Day 24 part 2

--- day_24.py
+++ day_24.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import networkx as nx
 from parse import parse
+import more_itertools as mit
 
 def build_graph(rawdata):
     G = nx.DiGraph()
@@ -11,15 +12,54 @@ def build_graph(rawdata):
         G.add_node(wire, value=int(value))
 
     for rule in rules.splitlines():
-        try:
-            l, op, r, out = parse("{} {} {} -> {}", rule)
-        except:
-            breakpoint()
+        l, op, r, out = parse("{} {} {} -> {}", rule)
         G.add_node(out, op=op)
         G.add_edge(l, out)
         G.add_edge(r, out)
 
     return G
+
+class LogicCircuit:
+    def __init__(self, rawdata):
+        self.G = build_graph(rawdata)
+        self.swapped = set()
+
+    def rename(self, old, new):
+        # print(f"RENAME {old}->{new}")
+        self.G.nodes[old]["old_name"] = old
+        nx.relabel_nodes(self.G, {old: new}, copy=False)
+
+    def swap(self, n1, n2):
+        # print(f"SWAP {n1}<=>{n2}")
+        if n1.startswith("z") or n2.startswith("z"):
+            # FIXME I dont quite understand why this difference is needed
+            # except that trial and error proved it works
+            G = nx.relabel_nodes(self.G, {n1: n2, n2: n1})
+            G.remove_edges_from(list(G.edges((n1, n2))))
+            G.add_edges_from((n1, e[1]) for e in self.G.edges(n1))
+            G.add_edges_from((n2, e[1]) for e in self.G.edges(n2))
+            self.G = G
+            self.swapped.add(self.G.nodes[n1].get("old_name", n1))
+            self.swapped.add(self.G.nodes[n2].get("old_name", n2))
+        else:
+            new_n1 = list(self.outputs(n2)) 
+            new_n2 = list(self.outputs(n1)) 
+            self.G.remove_edges_from(list(self.G.edges((n1,n2))))
+            self.G.add_edges_from((n1,n) for n in new_n1)
+            self.G.add_edges_from((n2,n) for n in new_n2)
+            self.swapped.add(self.G.nodes[n1].get("old_name", n1))
+            self.swapped.add(self.G.nodes[n2].get("old_name", n2))
+
+    def inputs(self, gate):
+        return sorted(e[0] for e in self.G.in_edges(gate))
+
+    def outputs(self, gate):
+        return sorted(e[1] for e in self.G.edges(gate))
+
+    @property
+    def uncorrected(self):
+        return (node for node in nx.topological_sort(self.G) if not node.startswith(("x", "y", "z", "AND", "XOR", "CARRY", "INTERCARRY")))
+
 
 ops = {
     "AND": lambda x,y: x and y ,
@@ -42,7 +82,49 @@ def part_1(rawdata):
     return res
 
 def part_2(rawdata):
-    pass
+    # Adapted from https://www.reddit.com/r/adventofcode/comments/1hnb969/2024_day_24_part_2_aliasing_wires_to_spot_the/
+    circuit = LogicCircuit(rawdata)
+    while True:
+        try:
+            node = mit.first(circuit.uncorrected)
+        except ValueError:
+            break
+        l, r = circuit.inputs(node)
+        op = circuit.G.nodes[node]["op"]
+
+        if l.startswith("x") and r.startswith("y"):
+            n = l.removeprefix("x")
+            assert n == r.removeprefix("y")
+
+            if n == "00" and op == "AND":
+                circuit.rename(node, "CARRY00")
+            else:
+                circuit.rename(node, f"{op}{n}")
+
+        elif l.startswith("AND") and r.startswith("INTERCARRY"):
+            n = l.removeprefix("AND")
+            assert r.removeprefix("INTERCARRY") == n
+            assert op == "OR"
+            circuit.rename(node, f"CARRY{n}")
+
+        elif l.startswith("CARRY") and r.startswith("XOR"):
+            # This should be CARRY(n-1) XOR XOR(N) => Z(N) 
+            #             or CARRY(n-1) AND XOR(N) -> INTERCARRY(N)
+            n = r.removeprefix("XOR")
+            assert int(l.removeprefix("CARRY")) == int(n) - 1
+
+            if op == "XOR" and not node.startswith("z"):
+                circuit.swap(node, f"z{n}")
+            if op == "AND":
+                circuit.rename(node, f"INTERCARRY{n}")
+
+        elif l.startswith("AND") and r.startswith("CARRY") and op == "AND":
+            n = int(r.removeprefix("CARRY"))
+            circuit.swap(l, f"XOR{n+1}")
+        else:
+            breakpoint()
+
+    return ",".join(sorted(circuit.swapped))
 
 from aocd import puzzle, submit
 import pytest
@@ -112,10 +194,10 @@ tnw OR pbm -> gnj""", 2024)])
 def test_part_1(data, result):
     assert part_1(data) == result
 
-@pytest.mark.parametrize("data, result",
-     [(ex.input_data, ex.answer_b) for ex in puzzle.examples])
-def test_part_2(data, result):
-    assert part_2(data) == result
+# @pytest.mark.parametrize("data, result",
+#      [(ex.input_data, ex.answer_b) for ex in puzzle.examples])
+# def test_part_2(data, result):
+#     assert part_2(data) == result
 
 if __name__ == "__main__":
     if (test_result := pytest.main([__file__])):
